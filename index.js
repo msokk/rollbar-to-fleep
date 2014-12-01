fc750d7be28b408df0d9e9de7a748fc52117bd1d
Mihkel Sokk <mihkelsokk@gmail.com>
2014-12-01 14:52:53 +0200

    Formatting and params enhancements

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var express = require('express'),
+    util = require('util'),
     bodyParser = require('body-parser'),
     app = express(),
     request = require('superagent');
 
 app.use(bodyParser.json());
-
-if(!process.env.HOOK_ID || !process.env.FLEEP_HOOK_ID) return;
-
-var github_repo = process.env.GITHUB_REPO || 'dummy/dummy';
-
 app.set('port', (process.env.PORT || 5000));
 
-app.post('/' + process.env.HOOK_ID, function(req, res) {
+// Secret not set - exit
+if(!process.env.SECRET) return;
+
+var deploy_template = '*%s* deployed to *%s* revision %s<<_%s_>>',
+    error_template = '*%s on %s*: %s. %s<<See more...>>';
+
+
+app.post('/:secret/:hook_id', function(req, res) {
   console.log(req.body);
   var msg = '',
       data = req.body.data;
@@ -19,22 +22,22 @@ app.post('/' + process.env.HOOK_ID, function(req, res) {
   if(req.body.event_name === 'deploy') {
     data = data.deploy;
 
-    msg = '*' + data.local_username + '* deployed *' + data.environment +
-      '* revision _' + data.revision.substring(0, 9) +
-      '_ \nhttps://github.com/' + github_repo + '/commit/' + data.revision;
+    msg = util.format(deploy_template, data.local_username, data.environment,
+      'https://github.com/' + req.param('github') + '/commit/' + data.revision,
+      data.revision.substring(0, 9));
   }
 
   if(req.body.event_name === 'new_item') {
     data = data.item;
-    var last_err = data.last_occurrence;
+    var last_err = data.last_occurrence,
+        level = last_err.level[0].toUpperCase() + last_err.level.slice(1);
 
-    msg = last_err.level[0].toUpperCase() + last_err.level.slice(1) +
-      ': ' + data.title + ' (' + data.environment + ')\n' +
-      'https://rollbar.com/item/uuid?uuid=' + last_err.uuid;
+    msg = util.format(error_template, level, data.environment, data.title,
+      'https://rollbar.com/item/uuid?uuid=' + last_err.uuid);
   }
 
   request
-    .post('https://fleep.io/hook/' + process.env.FLEEP_HOOK_ID)
+    .post('https://fleep.io/hook/' + req.param('hook_id'))
     .send({ message: msg })
     .end(function(error, response) {
       res.status(200).end();
